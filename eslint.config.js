a908089cc9df75ecffb91425eb3c6ba17a004f22
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-10-29 22:49:18 -0300

    chore: Configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+/* --- IMPORTS ---*/
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+
+/* --- EXPORTS ---*/
+export default tseslint.config(
+  // Ignore build output
+  { ignores: ["dist"] },
+
+  // TypeScript + React
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+);
